354896f5f20aa1c5b0549b006eebfe27bff974fd
CarlosFranchN <carlosaragaoneto@edu.unifor.br>
2023-12-12 23:49:26 -0300

    UpdatesFlask
    seguindo os passos do quickStart da pagina do docs do Flask

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
-from flask import * 
+from flask import Flask, render_template, request
 from markupsafe import escape
 
 app = Flask(__name__)
-# forma de identificar o principal
-@app.route('/')
-@app.route('/index')
-def index ():
-    nomesList = ["Carlos","Pedro", "Mari","Vitoria"]
-    return render_template('index.html', nomes = nomesList)
 
-@app.route('/sobre')
-def sobre():
-    return  render_template('sobre.html')
 
-@app.route('/hello/<name>')
-def helloP(name):
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/hello")
+def inicio():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/user/<username>")
+def show_user(username):
+    return f"<h1>User {escape(username)}</h1>"
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return f"Post {post_id}"
+
+
+@app.route("/hello/<name>")
+def hello(name):
     return f"<h1>Hello, {escape(name)}!!</h1>"
 
 
+@app.route("/projeto/")
+def projeto():
+    return "Project Page"
 
-if __name__ =='__main__':
+
+@app.route("/sobre")
+def sobre():
+    return "Sobre pag"
+
+
+@app.get("/login")
+def login_get():
+    return show_the_login_form()
+
+
+@app.route("/form")
+def pag():
+    return render_template("inicio.html")
+
+
+@app.post("/process_form")
+def form():
+    primeiro_nome = request.form["primeiro_nome"]
+    segundo_nome = request.form["segundo_nome"]
+
+    # Faça algo com os dados, como imprimir no console para este exemplo
+    print(f"Primeiro Nome: {primeiro_nome}, Segundo Nome: {segundo_nome}")
+
+    # Você pode redirecionar para outra página ou renderizar uma página de confirmação
+    return "Formulário enviado com sucesso!"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
     # testesr
-
-   
